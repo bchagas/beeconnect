b371862db9d6ab6af08620b5f8100f84f418456a
Bruno Chagas <brunochagasweb@gmail.com>
2015-07-19 17:58:47 -0300

    Fixed heroku config

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-require 'rack'
-require 'rack/contrib/try_static'
-
 ###
 # Compass
 ###
@@ -90,14 +87,3 @@ end
 
 #Sprockets config
 sprockets.import_asset 'logo@2x.png'
-
-# Serve files from the build directory
-use Rack::TryStatic,
-  root: 'build',
-  urls: %w[/],
-  try: ['.html', 'index.html', '/index.html']
-
-run lambda{ |env|
-  four_oh_four_page = File.expand_path("../build/404/index.html", __FILE__)
-  [ 404, { 'Content-Type'  => 'text/html'}, [ File.read(four_oh_four_page) ]]
-}
