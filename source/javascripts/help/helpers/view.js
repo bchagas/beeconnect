a3712043b73c33b5bac2d717192e6281ae3895fb
Bruno Chagas <brunochagasweb@gmail.com>
2015-08-10 22:17:12 -0300

    Updated string helper

--- source/javascripts/help/helpers/view.js
+++ source/javascripts/help/helpers/view.js
@@ -1,8 +1,30 @@
 Handlebars.registerHelper("parameterize", function(value) {
-  return value
-          .toLowerCase()
-          .replace(/[^a-z0-9]+/g,'-')
-          .replace(/(^-|-$)/g,'');
+  var specialChars = [
+    { val:"a",let:"áàãâä" },
+    { val:"e",let:"éèêë" },
+    { val:"i",let:"íìîï" },
+    { val:"o",let:"óòõôö" },
+    { val:"u",let:"úùûü" },
+    { val:"c",let:"ç" },
+    { val:"A",let:"ÁÀÃÂÄ" },
+    { val:"E",let:"ÉÈÊË" },
+    { val:"I",let:"ÍÌÎÏ" },
+    { val:"O",let:"ÓÒÕÔÖ" },
+    { val:"U",let:"ÚÙÛÜ" },
+    { val:"C",let:"Ç" },
+    { val:"",let:"?!()" }
+  ];
+    var $spaceSymbol = '-';
+    var regex;
+    var returnString = value;
+    for (var i = 0; i < specialChars.length; i++) {
+      regex = new RegExp("["+specialChars[i].let+"]", "g");
+      returnString = returnString.replace(regex, specialChars[i].val);
+      regex = null;
+    }
+    return returnString
+            .replace(/\s/g, $spaceSymbol)
+            .toLowerCase();
 });
 
 Handlebars.registerHelper('link', function(text, url) {
