d0cf0abbbc747601eecbd0ca877e6a886d2936ad
Bruno Chagas <brunochagasweb@gmail.com>
2015-08-28 12:51:00 -0300

    Added home page menu

---
+++ source/javascripts/mobile-navigation.js
@@ -0,0 +1,45 @@
+beeConnect.mobileNavigation = function(menu, trigger) {
+  menu = $(menu); trigger = $(trigger);
+
+  var visibleAreaHeight = $(window).height(),
+      logo = $(".mobile-logo"),
+      page = $("body");
+
+  menu.css({ "margin-top" : -Math.abs(visibleAreaHeight) });
+  logo.css({ "margin-top" : -Math.abs(visibleAreaHeight) });
+
+  trigger.on("click", function(event) {
+    trigger.$ = $(this);
+
+    if(!menu.is(":visible")) {
+      page.css({ overflow: "hidden" });
+      menu.show();
+      logo.css({ visibility: "visible" });
+
+      menu.animate({
+        "margin-top" : 0,
+      }, 500);
+
+      logo.animate({
+        "margin-top" : "5%",
+      }, 200);
+
+      trigger.$.addClass("active");
+    } else {
+      trigger.$.removeClass("active");
+      page.removeAttr("style");
+
+      logo.animate({
+        "margin-top" : -Math.abs(visibleAreaHeight),
+      }, 500).css({"visibility" : "hidden"});
+
+      menu.animate({
+        "margin-top" : -Math.abs(visibleAreaHeight),
+      }, 200, function(){
+        menu.hide();
+      });
+
+      trigger.$.removeClass("active");
+    }
+  });
+};
